46efd9567c60a3f66a7955bdacb883ebfbb0efdd
JoeM <joe.moloney496@gmail.com>
2021-04-01 11:03:54 +0100

    Initial commit

---
+++ CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //Usage: 3D & 2D
+    //Comment Line 25 For 2D
+
+    //Use Transform to get info about position, rotation & scale
+    public Transform target;
+    public float smoothSpeed = 1f;
+    public Vector3 offset; //Value to offset camera by relative to player
+
+    //Run camera follow function after movement function has been made in Update() > See PlayerMovement Script
+    private void FixedUpdate()
+    {
+        //Transform position of object script is attached to to target coords
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime); //CurrentPosition, TargetPosition, TimeToReachPosition
+
+        transform.position = smoothPosition;
+
+        //Rotate Camera Around Y-Axis
+        transform.LookAt(target);
+    }
+}
---
+++ MouseFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    //Usage 3D & 2D
+    float delay = 3.3f;
+    void LateUpdate()
+    {
+        Vector2 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler((180 + angle), -90, -90);
+
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(angle, -90, 0), 5);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(180 + angle, -90, -90), delay);
+    }
+}
