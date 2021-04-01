91ab7c650b23197349e8047470833997de60744e
JoeM <joe.moloney496@gmail.com>
2021-04-01 16:41:55 +0100

    Added Comments + Methods [SimpleGrapplingHook2D]

--- SimpleGrapplingHook2D.cs
+++ SimpleGrapplingHook2D.cs
@@ -11,7 +11,7 @@ public class SimpleGrapplingHook2D : MonoBehaviour
 
     void Start()
     {
-        _distanceJoint.enabled = false; //Disable joint on script load
+        _distanceJoint.enabled = false; //Disable Rigidbody2D Joint on script load
     }
 
     // Update is called once per frame
@@ -19,25 +19,36 @@ public class SimpleGrapplingHook2D : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)) //On Left Click
         {
-            Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition); //Use X & Y Axis Of Screen
-            _lineRenderer.SetPosition(0, mousePos); //Get position of mouse click
-            _lineRenderer.SetPosition(1, transform.position); //Get position of object script is attached to
-            _distanceJoint.connectedAnchor = mousePos; //Anchors first object to mouse click location
-            _lineRenderer.enabled = true; //Enable the line to be drawn
-            _distanceJoint.enabled = true; //Enable the Rigidbody2D Link
+            StopCoroutine(LineRetract(time)); //Stop LineRetract Coroutine
+            GetPositions(); //Get Coords & Enable LineRenderer + Rigidbody2D Joint
             StartCoroutine(LineDraw(time)); //Animate the line being drawn 
         }
         else if (Input.GetKeyUp(KeyCode.Mouse0)) //On Left Click Release
         {
+            StopCoroutine(LineDraw(time)); //Stop LineDraw Coroutine
             StartCoroutine(LineRetract(time)); //Animate line retracting
         }
-        if (_distanceJoint.enabled)
-        {
-            _lineRenderer.SetPosition(1, transform.position);
-        }
+        if (_distanceJoint.enabled) //If Rigidbody2D Joint Is Enabled
+            _lineRenderer.SetPosition(1, transform.position); //Set LineRenderer To Follow Connected Object
     }
 
-    IEnumerator LineDraw(float time)
+    public void GetPositions() //Get Coords & Enable LineRenderer + Rigidbody2D Joint
+    {
+        Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition); //Use X & Y Axis Of Screen
+        _lineRenderer.SetPosition(0, mousePos); //Get position of mouse click
+        _lineRenderer.SetPosition(1, transform.position); //Get position of object script is attached to
+        _distanceJoint.connectedAnchor = mousePos; //Anchors first object to mouse click location
+        _lineRenderer.enabled = true; //Enable the line to be drawn
+        _distanceJoint.enabled = true; //Enable the Rigidbody2D Link
+    }
+
+    public void ReleasePositions() //Disable LineRenderer & RigidBody2D Joint
+    {
+        _lineRenderer.enabled = false; //Disable the line to be drawn
+        _distanceJoint.enabled = false; //Disable the Rigidbody2D Link
+    }
+
+    IEnumerator LineDraw(float time) //Draw Line From Object To Mouse Position
     {
         float t = 0;
         Vector3 orig = _lineRenderer.GetPosition(0); //Mouse Position
@@ -53,7 +64,7 @@ public class SimpleGrapplingHook2D : MonoBehaviour
         }
         _lineRenderer.SetPosition(0, orig); //Set New Mouse Position For Line Renderer
     }
-    IEnumerator LineRetract(float time)
+    IEnumerator LineRetract(float time) //Retract Line From Mouse Position To Object
     {
         float t = 0;
         Vector3 orig = _lineRenderer.GetPosition(0); //Mouse Position
@@ -68,43 +79,6 @@ public class SimpleGrapplingHook2D : MonoBehaviour
             yield return null;
         }
         _lineRenderer.SetPosition(0, orig2); //Set Object Position For Line Renderer
-        _distanceJoint.enabled = false; //Disable The Rigidbody2D Link
-        _lineRenderer.enabled = false; //Disable The LineRenderer
+        ReleasePositions(); //Disable LineRenderer & RigidBody2D Joint
     }
-
-    //IEnumerator LineDraw(float time)
-    //{
-    //    float t = 0;
-    //    Vector3 orig = _lineRenderer.GetPosition(0);
-    //    Vector3 orig2 = _lineRenderer.GetPosition(1);
-    //    _lineRenderer.SetPosition(1, orig);
-    //    Vector3 newPos;
-
-    //    for (; t < time; t += Time.deltaTime)
-    //    {
-    //        newPos = Vector3.Lerp(orig, orig2, t / time);
-    //        _lineRenderer.SetPosition(1, newPos);
-    //        yield return null;
-    //    }
-    //    _lineRenderer.SetPosition(1, orig2);
-    //    _distanceJoint.enabled = true;
-    //}
-    //IEnumerator LineRetract(float time)
-    //{
-    //    float t = 0;
-    //    Vector3 orig = _lineRenderer.GetPosition(0);
-    //    Vector3 orig2 = _lineRenderer.GetPosition(1);
-    //    _lineRenderer.SetPosition(1, orig);
-    //    Vector3 newPos;
-
-    //    _distanceJoint.enabled = false;
-    //    for (; t < time; t += Time.deltaTime)
-    //    {
-    //        newPos = Vector3.Lerp(orig2, orig, t / time);
-    //        _lineRenderer.SetPosition(1, newPos);
-    //        yield return null;
-    //    }
-    //    _lineRenderer.SetPosition(1, orig2);
-    //    _lineRenderer.enabled = false;
-    //}
 }
